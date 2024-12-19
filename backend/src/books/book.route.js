e4312cb7fba12a4e535de2bd3904802035f791ff
A.I.P.Jayasooriya <isurupromuditha617@gmail.com>
2024-12-19 19:54:40 +0530

    Create admin & verify token

--- backend/src/books/book.route.js
+++ backend/src/books/book.route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const Book = require('./book.model');
 const { postABook, getAllBooks, getSingleBook, updateBook, deleteABook } = require('./book.controller');
+const verifyAdminToken = require('../middleware/verifyAdminToken');
 const router = express.Router();
 
 // frontend => backend server => controller => book schema => database => send to server => back to the frontend
@@ -11,7 +12,7 @@ const router = express.Router();
 // delete = when delete something
 
 //post a book
-router.post('/create-book', postABook)
+router.post('/create-book', verifyAdminToken, postABook)
 
 //get all books
 router.get('/', getAllBooks)
@@ -20,9 +21,9 @@ router.get('/', getAllBooks)
 router.get('/:id', getSingleBook)
 
 //update book endpoint
-router.put('/edit/:id', updateBook)
+router.put('/edit/:id', verifyAdminToken, updateBook)
 
 //delete a book
-router.delete('/:id', deleteABook)
+router.delete('/:id', verifyAdminToken, deleteABook)
 
 module.exports = router;
